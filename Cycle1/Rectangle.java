afe45868b0a68fd4f7299109cd44ba1fe7c82a6b
Aravind Ashokan <77882744+code-lover636@users.noreply.github.com>
2023-10-04 20:48:43 +0530

    Create Rectangle.java

---
+++ Cycle1/Rectangle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Peri{
+	double length, breadth;
+	Peri(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter length: ");
+		length = sc.nextDouble();
+		System.out.print("Enter breadth: ");
+		breadth = sc.nextDouble();
+	}
+	Peri(double l, double b){
+		length = l;
+		breadth = b;
+	}
+	void calculate(){
+		System.out.println("Perimeter is "+2*(length+breadth));
+	}
+}
+
+public class Rectangle{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		Peri rect1 = new Peri();
+		rect1.calculate();
+		System.out.print("Enter length: ");
+		double l = sc.nextDouble();
+		System.out.print("Enter breadth: ");
+		double b = sc.nextDouble();
+		Peri rect2 = new Peri(l, b);
+		rect2.calculate();
+	}
+}
