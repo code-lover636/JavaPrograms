6d5035a83a7bd1b226d7ec050db819d8a0bc65de
Aravind Ashokan <77882744+code-lover636@users.noreply.github.com>
2023-11-27 20:41:13 +0530

    Update Palindrome.java

--- Cycle2/Palindrome.java
+++ Cycle2/Palindrome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome{
+class Palindrome{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter string: ");
@@ -8,10 +8,11 @@ public class Palindrome{
 		
 		for(int i=0; i<str.length()/2; i++){
 			if(str.charAt(i) != str.charAt(str.length()-i-1)){
-				System.out.println("Not Palindrome.");
+				System.out.println(str + " is not a palindrome.");
 				return;
-			
+			}
 		}
-		System.out.println("Palindrome.");
+		
+		System.out.println(str + " is a palindrome.");
 	}
 }
