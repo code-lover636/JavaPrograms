ea03becd88ed6dc60356248d172e13647d14876e
Aravind Ashokan <77882744+code-lover636@users.noreply.github.com>
2023-11-28 21:04:17 +0530

    Update OfficerManager.java

--- Cycle4/OfficerManager.java
+++ Cycle4/OfficerManager.java
@@ -14,33 +14,29 @@ class Employee{
 
 class Officer extends Employee{
 	String specialization;
-	String department;
-	Officer(String name, int age, long phNo, String address, double salary, String spec, String dep){
+	Officer(String name, int age, long phNo, String address, double salary, String spec){
 		super.name = name;
 		super.age = age;
 		super.phNo = phNo;
 		super.address = address;
 		super.salary = salary;
 		this.specialization = spec;
-		this.department = dep;
 	}
 }
 
 class Manager extends Employee{
-	String specialization;
 	String department;
-	Manager(String name, int age, long phNo, String address, double salary, String spec, String dep){
+	Manager(String name, int age, long phNo, String address, double salary, String dep){
 		super.name = name;
 		super.age = age;
 		super.phNo = phNo;
 		super.address = address;
 		super.salary = salary;
-		this.specialization = spec;
 		this.department = dep;
 	}
 }
 
-public class Hier{
+class Hier{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		
@@ -50,8 +46,6 @@ public class Hier{
 		String name = sc.nextLine();
 		System.out.print("Enter specialization: ");
 		String spec = sc.nextLine();
-		System.out.print("Enter department: ");
-		String dep = sc.nextLine();
 		System.out.print("Enter address: ");
 		String address = sc.nextLine();
 		System.out.print("Enter age: ");
@@ -61,16 +55,14 @@ public class Hier{
 		System.out.print("Enter salary: ");
 		double salary = sc.nextDouble();
 		sc.nextLine();
-		Officer o1 = new Officer(name, age, phNo, address, salary, spec, dep);
+		Officer o1 = new Officer(name, age, phNo, address, salary, spec);
 		
 		//Manager
-		System.out.println("Manager: ");
+		System.out.println("\nManager: ");
 		System.out.print("Enter name: ");
 		name = sc.nextLine();
-		System.out.print("Enter specialization: ");
-		spec = sc.nextLine();
 		System.out.print("Enter department: ");
-		dep = sc.nextLine();
+		String dep = sc.nextLine();
 		System.out.print("Enter address: ");
 		address = sc.nextLine();
 		System.out.print("Enter age: ");
@@ -79,9 +71,12 @@ public class Hier{
 		phNo = sc.nextLong();
 		System.out.print("Enter salary: ");
 		salary = sc.nextDouble();
-		Manager m1 = new Manager(name, age, phNo, address, salary, spec, dep);
+		Manager m1 = new Manager(name, age, phNo, address, salary, dep);
 		
-		System.out.println("OFFICER: Name: "+o1.name+",Age: "+o1.age+", phNo: "+o1.phNo+", Address: "+o1.address+", Salary: "+o1.salary);
-		System.out.println("MANAGER: Name: "+m1.name+",Age: "+m1.age+", phNo: "+m1.phNo+", Address: "+m1.address+", Salary: "+m1.salary);
+		o1.printSalary();
+		m1.printSalary();
+		
+		System.out.println("\nOFFICER:\nName: "+o1.name+",\nAge: "+o1.age+",\nphNo: "+o1.phNo+",\nAddress: "+o1.address+",\nSalary: "+o1.salary);
+		System.out.println("\nMANAGER:\nName: "+m1.name+",\nAge: "+m1.age+",\nphNo: "+m1.phNo+",\nAddress: "+m1.address+",\nSalary: "+m1.salary);
 	}
 }
