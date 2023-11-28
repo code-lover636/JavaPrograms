dc5cbf2ab4715ba1d6cb513c8612bb64eca27016
Aravind Ashokan <77882744+code-lover636@users.noreply.github.com>
2023-11-28 06:43:31 +0530

    Update Palindrome.java

--- Cycle2/Palindrome.java
+++ Cycle2/Palindrome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Palindrome{
+public class PalindromeCheck{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter string: ");
